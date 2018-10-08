6ced2cfe0f632cfd3221902ec39a26b56398e161
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-10-07 20:41:06 -0400

    Finish event system lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+ 
+    good = () => {
+      console.log('Good!');
+    }
+
+    hey = () => {
+        console.log('Hey! Eyes on me!')
+    }
+   
+    render() {
+      return (
+        <div><button onFocus={this.good} onBlur={this.hey}>Click me!</button></div>
+      );
+    }
+  }
+
+  export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+ 
+    foo = () => {
+      console.log('Entering password...');
+    }
+   
+    render() {
+      return (
+        <div><input type="password" onKeyUp={this.foo}></input></div>
+      );
+    }
+  }
+
+  export default Keypad;
